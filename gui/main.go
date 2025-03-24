6787a4a6a2cf79c7dc29d06d6810121a250aa6bc
Rico Aurelio Gunadi S <100511874+rcarlgnds@users.noreply.github.com>
2025-03-24 21:41:47 +0700

    Feature: OS Recognition

--- gui/main.go
+++ gui/main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"runtime"
 	"sIOmay/gui/pages"
 )
 
 func main() {
+	os := runtime.GOOS
+
+	switch os {
+	case "darwin":
+		fmt.Println("Mac OS")
+	case "linux":
+		fmt.Println("Linux")
+	case "windows":
+		fmt.Println("windows")
+	default:
+		fmt.Println("Unknown OS")
+	}
+
 	application := app.New()
 
 	window := application.NewWindow("sIOmay 🥟")
