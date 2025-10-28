530753cf6f30e1bb64c1981a9a938d868506c591
JojmoZ <vincentiusjonathantanujaya@gmail.com>
2025-10-28 14:38:08 +0700

    fix: modify UI for UX

--- gui/pages/opening.go
+++ gui/pages/opening.go
@@ -2,21 +2,8 @@ package pages
 
 import (
 	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/theme"
-	"fyne.io/fyne/v2/widget"
 )
 
 func Opening(window fyne.Window) fyne.CanvasObject {
-	autoButton := widget.NewButtonWithIcon("  Auto Mode  ", theme.DesktopIcon(), func() {
-		window.SetContent(AutoControlPanel(window))
-	})
-	manualButton := widget.NewButtonWithIcon("  Manual Mode  ", theme.BrokenImageIcon(), func() {
-		window.SetContent(ManualControlPanel(window))
-	})
-
-	autoButton.Resize(fyne.NewSize(200, 400))
-	center := container.NewHSplit(autoButton, manualButton)
-
-	return container.NewCenter(center)
+	return ManualControlPanel(window)
 }
